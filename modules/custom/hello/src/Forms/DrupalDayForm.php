a5c0af05aef946367bbcdbeb6284a9f2a95fdee0
Grzegorz Bartman <grzegorz.bartman@droptica.com>
2015-11-21 12:15:06 +0100

    Issue #1: Form example. Base class.

---
+++ modules/custom/hello/src/Forms/DrupalDayForm.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\amazing_forms\Form\ContributeForm.
+ */
+
+// Example: http://www.trellon.com/content/blog/how-create-custom-form-in-drupal-8
+
+namespace Drupal\hello_world\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\UrlHelper;
+
+/**
+ * Contribute form.
+ */
+class DrupalDayForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+  }
+}
+?>
